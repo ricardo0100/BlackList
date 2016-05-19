d9421fc35f61689b0e14038233003f95cc22a690
Ricardo Gehrke Filho <ricardo0100@gmail.com>
2016-05-19 00:40:22 -0300

    Delete Person

--- HitList/PeopleViewController.swift
+++ HitList/PeopleViewController.swift
@@ -11,37 +11,44 @@ import CoreData
 
 class PeopleViewController: UITableViewController {
     
+    var managedContext: NSManagedObjectContext?
+    
     var people = [Person]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        managedContext = appDelegate.managedObjectContext
+    }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
-        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-        
-        let managedContext = appDelegate.managedObjectContext
-        
         let fetchRequest = NSFetchRequest(entityName: "Person")
         
         do {
-            let results = try managedContext.executeFetchRequest(fetchRequest)
+            let results = try managedContext!.executeFetchRequest(fetchRequest)
             people = results as! [Person]
         } catch let error as NSError {
             print("Could not fetch \(error), \(error.userInfo)")
         }
     }
     
-    // MARK: UITableViewDataSource
-    override func tableView(tableView: UITableView,
-                   numberOfRowsInSection section: Int) -> Int {
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return people.count
     }
     
-    override func tableView(tableView: UITableView,
-                   cellForRowAtIndexPath
-        indexPath: NSIndexPath) -> UITableViewCell {
-        
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            let person = people[indexPath.row]
+            people.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            deletePerson(person)
+        }
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Person Cell")
-        
         let person = people[indexPath.row]
         
         cell!.textLabel!.textColor = UIColor.whiteColor()
@@ -49,19 +56,18 @@ class PeopleViewController: UITableViewController {
         
         return cell!
     }
+    
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
 
     @IBAction func addName(sender: AnyObject) {
-        let alert = UIAlertController(title: "New Name",
-                                      message: "Add a new name",
-                                      preferredStyle: .Alert)
+        let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .Alert)
         
-        let saveAction = UIAlertAction(title: "Save",
-                                       style: .Default,
-                                       handler: { (action:UIAlertAction) -> Void in
-                                        
-                                        let textField = alert.textFields!.first
-                                        self.saveName(textField!.text!)
-                                        self.tableView.reloadData()
+        let saveAction = UIAlertAction(title: "Save", style: .Default, handler: { (action:UIAlertAction) -> Void in
+            let textField = alert.textFields!.first
+            self.savePersonWithName(textField!.text!)
+            self.tableView.reloadData()
         })
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .Default, handler: nil)
@@ -72,29 +78,31 @@ class PeopleViewController: UITableViewController {
         alert.addAction(saveAction)
         alert.addAction(cancelAction)
         
-        presentViewController(alert,
-                              animated: true,
-                              completion: nil)
+        presentViewController(alert, animated: true, completion: nil)
     }
     
-    func saveName(name: String) {
-        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-        
-        let managedContext = appDelegate.managedObjectContext
-        
-        let entity =  NSEntityDescription.entityForName("Person", inManagedObjectContext:managedContext)
+    func savePersonWithName(name: String) {
+        let entity =  NSEntityDescription.entityForName("Person", inManagedObjectContext:managedContext!)
         
         let person = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext) as! Person
         
         person.name = name
         
         do {
-            try managedContext.save()
+            try managedContext!.save()
             people.append(person)
         } catch let error as NSError  {
             print("Could not save \(error), \(error.userInfo)")
         }
     }
-
+    
+    func deletePerson(person: Person) {
+        managedContext!.deleteObject(person)
+        do {
+            try managedContext!.save()
+        } catch let error as NSError  {
+            print("Could not delete \(error), \(error.userInfo)")
+        }
+    }
 }
 
