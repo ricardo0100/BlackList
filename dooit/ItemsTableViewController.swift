c4ccf9b27edd465eac3a8f4c114a3740806dde5c
Ricardo Gehrke Filho <ricardo0100@gmail.com>
2016-05-24 11:40:42 -0300

    Travis Again 🙄

--- dooit/ItemsTableViewController.swift
+++ dooit/ItemsTableViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ReasonsTableViewController.swift
-//  BlackList
+//  dooit
 //
 //  Created by Ricardo Gehrke Filho on 19/05/16.
 //  Copyright © 2016 Ricardo Gehrke Filho. All rights reserved.
