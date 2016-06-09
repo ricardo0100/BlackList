3947d239db2c35ea7ec363084c86667d8d409f4d
Ricardo Gehrke Filho <ricardo0100@gmail.com>
2016-06-08 23:55:41 -0300

    Project organization

---
+++ dooit/ViewModel/DeleteItemFromListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  DeleteItemFromListViewModel.swift
+//  dooit
+//
+//  Created by Ricardo Gehrke Filho on 08/06/16.
+//  Copyright © 2016 Ricardo Gehrke Filho. All rights reserved.
+//
+
+import Foundation
+
+class DeleteItemFromListViewModel {
+    
+}
