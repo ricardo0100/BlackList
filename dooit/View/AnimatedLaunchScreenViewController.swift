3f48b048f6d991d6a69db0772ee0870df92133c9
Ricardo Gehrke Filho <ricardo0100@gmail.com>
2016-07-13 22:14:39 -0300

    LaunchScreen animation adjustments

--- dooit/View/AnimatedLaunchScreenViewController.swift
+++ dooit/View/AnimatedLaunchScreenViewController.swift
@@ -19,26 +19,26 @@ class AnimatedLaunchScreenViewController: UIViewController {
         super.viewDidLoad()
         
         self.label1.transform = CGAffineTransformScale(self.label1.transform, 1, 1)
-        UIView.animateWithDuration(0.5, delay: 0.0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+        UIView.animateWithDuration(0.5, delay: 0.5, options: UIViewAnimationOptions.CurveEaseIn, animations: {
             self.label1.transform = CGAffineTransformScale(self.label1.transform, 0.5, 0.5)
             self.label1.alpha = 0.0
         }, completion: nil)
         
         self.label2.transform = CGAffineTransformScale(self.label2.transform, 1, 1)
-        UIView.animateWithDuration(0.5, delay: 0.2, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+        UIView.animateWithDuration(0.5, delay: 0.7, options: UIViewAnimationOptions.CurveEaseIn, animations: {
             self.label2.transform = CGAffineTransformScale(self.label2.transform, 0.5, 0.5)
             self.label2.alpha = 0.0
             }, completion: nil)
         
         self.label3.transform = CGAffineTransformScale(self.label3.transform, 1, 1)
-        UIView.animateWithDuration(0.5, delay: 0.4, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+        UIView.animateWithDuration(0.5, delay: 0.9, options: UIViewAnimationOptions.CurveEaseIn, animations: {
             self.label3.transform = CGAffineTransformScale(self.label3.transform, 0.5, 0.5)
             self.label3.alpha = 0.0
             }, completion: nil)
         
         self.label4.transform = CGAffineTransformScale(self.label4.transform, 1, 1)
-        UIView.animateWithDuration(0.5, delay: 0.8, options: UIViewAnimationOptions.CurveLinear, animations: {
-            self.label4.transform = CGAffineTransformScale(self.label4.transform, 2.5, 2.5)
+        UIView.animateWithDuration(0.5, delay: 1.1, options: UIViewAnimationOptions.CurveLinear, animations: {
+            self.label4.transform = CGAffineTransformScale(self.label4.transform, 3.5, 3.5)
             self.label4.alpha = 0.0
         }) { (finished) in
             self.performSegueWithIdentifier("Start App", sender: nil)
